d27ac71ea5d88bb9fca2846611e0a8a8572adf1f
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-03-08 21:42:14 -0500

    strings

---
+++ primativeDataTypes/strings/strings.js
@@ -0,0 +1,43 @@
+console.log('String:', String);
+console.log('String:', String.prototype);
+console.log('String1:', Object.getOwnPropertyNames(String.prototype));
+
+
+//Write a function that takes a string as input and returns the string reversed.
+function reverseString(str) {
+    
+}
+console.log(reverseString("hello"));
+
+
+//Write a function that takes a string as input and returns true if it's a palindrome, 
+//false otherwise.
+function isPalindrome(str) {
+    
+}
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("hello")); // false
+
+//Write a function that counts the number of vowels in a string.
+function countVowels(str) {
+
+}
+console.log(countVowels("javascript")); // 3
+
+//Write a function that returns the longest word in a sentence.
+function longestWord(sentence) {
+   
+}
+console.log(longestWord("I love JavaScript programming")); // "programming"
+
+//Write a function that removes duolicates from a string.
+function removeDuplicates(str) {
+    
+}
+console.log(removeDuplicates("hello")); //  "helo"
+
+
+
+
+
+
