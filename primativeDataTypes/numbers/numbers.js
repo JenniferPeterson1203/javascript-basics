19ec23e821a030a428982658a471992c98b1be2c
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-03-09 22:39:47 -0400

    numbers

---
+++ primativeDataTypes/numbers/numbers.js
@@ -0,0 +1,32 @@
+//Write a function that returns whether a number is even or odd
+function isEvenOrOdd(num) {
+    
+}
+console.log(isEvenOrOdd(4)); // "Even"
+console.log(isEvenOrOdd(7)); // "Odd"
+
+//Write a function that returns the sum of all numbers from 1 to n
+function sumUpToN(n) {
+    
+}
+console.log(sumUpToN(5)); // 15 (1 + 2 + 3 + 4 + 5)
+
+//Reverse a number
+function reverseNumber(num) {
+    
+}
+console.log(reverseNumber(12345)); // 54321
+console.log(reverseNumber(-987)); // -789
+
+//Count the number of digits in a number
+function countDigits(num) {
+    
+}
+console.log(countDigits(12345)); // 5
+
+//Find the sum of the digits in a number
+function sumOfDigits(num) {
+    
+}
+console.log(sumOfDigits(123)); // 6 (1 + 2 + 3)
+
