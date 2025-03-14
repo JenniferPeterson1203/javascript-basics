4822d56b30d86f285851514d72dbd3fde13b89a2
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-03-14 13:29:24 -0400

    problems

--- WhatIsJavascript/dynamicVsStaticallyTyped.js
+++ WhatIsJavascript/dynamicVsStaticallyTyped.js
@@ -5,8 +5,14 @@ console.log('55' - 10);
 console.log('55' / 5);
 console.log('55' + false);
 console.log('55' - true);
+console.log(55 - true);
+console.log(55 - false)
+console.log(false / 55)
+console.log(true / 2);
+console.log('true' * true);
 console.log(true + 2);
 console.log(true + false);
 console.log(true - true);
+
 
 
