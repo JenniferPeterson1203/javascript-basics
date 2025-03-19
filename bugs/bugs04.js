99eaa728929958fab7744d068c93bca2315d0d46
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-03-19 17:41:31 -0400

    bug

--- bugs/bugs04.js
+++ bugs/bugs04.js
@@ -1,14 +1,14 @@
 //  Modify in place, any number that is a multiple of 3, replace it with it's index times 3
 // Do not change the forEach function
 
-// function replaceMultiplesOfThree(arr) {
-//     arr.forEach((num, index) => {
-//         if (num % 3 === 0) {
-//             num = index * 3; 
-//         }
-//     });
-//     return arr;
-// }
+function replaceMultiplesOfThree(arr) {
+    arr.forEach((num, index) => {
+        if (num % 3 === 0) {
+            num = index * 3; 
+        }
+    });
+    return arr;
+}
 
 // const numbers = [3, 5, 9, 12, 7, 15];
 // console.log('replaceMultiplesOfThree:', replaceMultiplesOfThree(numbers)); 
